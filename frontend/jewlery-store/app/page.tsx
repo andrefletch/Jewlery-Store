03b7a69d5599020714fad0aff98e4a571dd9b675
andrefletch <dfletcherr999@gmail.com>
2026-06-01 14:50:41 -0400

    added navigation routes

---
+++ frontend/jewlery-store/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+    <section className="max-w-7xl mx-auto px-6 py-20">
+      <h1 className="text-5xl font-bold">
+        New Season Collection
+      </h1>
+    </section>
+    )
+}
